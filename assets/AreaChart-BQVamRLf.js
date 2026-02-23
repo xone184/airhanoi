f98874837a05be36d8868c5cc2a9c6cd3bf7ccaa
xone184 <mvtho2004@gmail.com>
2026-02-23 15:10:05 +0700

    Updates

---
+++ assets/AreaChart-BQVamRLf.js
@@ -0,0 +1,1 @@
+import{E as a,X as r,Y as s,H as i}from"./generateCategoricalChart-E1kvAaOu.js";import{A as x}from"./Area-754jGsY7.js";var o=a({chartName:"AreaChart",GraphicalChild:x,axisComponents:[{axisType:"xAxis",AxisComp:r},{axisType:"yAxis",AxisComp:s}],formatAxisMap:i});export{o as A};
